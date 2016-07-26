c3856511f9b98e596f82c09471ddec9c70722cc2
Oliveira, Fernando <ernando.pedrazzanni@thomsonreuters.com>
2016-07-26 20:49:44 -0300

    List View and translate files (pt-BR, es)

--- Android/pub-customer/PubCustomerProj/src/main/java/com/pub/pub_customer/MainActivity.java
+++ Android/pub-customer/PubCustomerProj/src/main/java/com/pub/pub_customer/MainActivity.java
@@ -2,9 +2,8 @@ package com.pub.pub_customer;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +11,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,17 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        /*List view*/
+
+
+        mListView = (ListView) findViewById(R.id.listView);
+
+        String[] names = new String[] { "Android", "Windows7", "Symbian", "iPhone",
+                "Android", "Windows7", "Symbian", "iPhone",
+                "Android", "Windows7", "Symbian", "iPhone" };
+
+        mListView.setAdapter(new ArrayAdapter<String>(this, R.layout.content_main,R.id.textView2, names));
     }
 
     @Override
